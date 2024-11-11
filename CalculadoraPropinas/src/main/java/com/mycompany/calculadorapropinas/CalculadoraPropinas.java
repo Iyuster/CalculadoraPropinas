0b067b9f3019f6052c8bf6eaea4dbad2c648530c
Iyuster <iagogarciaiglesias@gmail.com>
2024-11-11 20:11:28 +0100

    commit 1

---
+++ CalculadoraPropinas/src/main/java/com/mycompany/calculadorapropinas/CalculadoraPropinas.java
@@ -0,0 +1,10 @@
+package com.mycompany.calculadorapropinas;
+/*
+ * @author Iago Garcia Iglesias
+ */
+public class CalculadoraPropinas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
